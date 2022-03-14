cdf4169f0547e407ecc2939f0df6074405743dab
Ione Dias <lobodias.ione@gmail.com>
2022-03-14 13:58:54 -0300

    meu primeiro commit do cypress

---
+++ cypress/factories/SignupFactory.js
@@ -0,0 +1,31 @@
+var faker = require('faker')
+var cpf = require('gerador-validador-cpf')
+
+export default {
+
+    deliver: function () {
+
+        var firstName = faker.name.firstName()
+        var lastName = faker.name.lastName()
+        
+        var data = {
+            name: `${firstName} ${lastName}`,
+            cpf: cpf.generate(),
+            email: faker.internet.email(firstName),
+            whatsapp: '11999999999',
+            address: {
+                postalcode: '05336010',
+                street: 'Rua Floresto Bandecchi',
+                number: '479',
+                details: 'Ap 142',
+                district: 'Jaguaré',
+                city_state: 'São Paulo/SP'
+            },
+            delivery_method: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }
+
+        return data
+    }
+
+}
